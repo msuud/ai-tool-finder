39834e05ce67b7fa54c8bab13eb73b8c407f6449
msuudoct2@gmail.com <msuudoct2@gmail.com>
2024-05-16 17:44:16 +0530

    background image changed

--- src/components/CustomNavbar/CustomNavbar.js
+++ src/components/CustomNavbar/CustomNavbar.js
@@ -1,33 +1,33 @@
 import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
 import Logo from "../../assets/logo.jpg";
+import "./CustomNavbar.css";
 
 const CustomNavbar = () => {
   return (
-    <nav class="navbar navbar-expand-sm bg-black opacity-10">
-      <div class="container-fluid">
+    <nav class="navbar navbar-expand-sm bkg">
+      <div class="container-fluid ">
         <a class="navbar-brand" href="#">
           <img src={Logo} style={{ width: "50px" }} class="rounded-pill"></img>
         </a>
-        <ul class="navbar-nav" style={{ marginLeft: "auto" }}>
+        <ul class="navbar-nav ">
           <li class="nav-item">
             <a class="nav-link text-white pe-5" href="#">
-              Home
+              HOME
             </a>
           </li>
           <li class="nav-item ">
             <a class="nav-link text-white pe-5" href="#">
-              AI Tools
+              AI TOOLS
             </a>
           </li>
           <li class="nav-item ">
             <a class="nav-link text-white pe-5" href="#">
-              Blog
+              BLOG
             </a>
           </li>
           <li class="nav-item ">
             <a class="nav-link text-white pe-5" href="#">
-              Login
+              LOGIN
             </a>
           </li>
         </ul>
--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -4,10 +4,7 @@ import "./HomePage.css";
 const HomePage = () => {
   return (
     <div className="container-fluid bg-image">
-      <div className="container text-black ml-0">
-        <p className="h1">hello world </p>
-        <p>this is a line</p>
-      </div>
+      <div className="container text-black ml-0"></div>
     </div>
   );
 };
