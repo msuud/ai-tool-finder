9e9373345e6e6939d20d2bf980b5c3dfa9aedb32
Kirtan Patel <kirtan251608@gmail.com>
2024-05-16 15:46:45 +0530

    added footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="container text-white ml-0">
       <p className="h1">hello world </p>
       <p>this is a line</p>
+      <Footer/>
     </div>
   );
 }
