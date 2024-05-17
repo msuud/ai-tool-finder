bbba9ac870c3f46d2cad655e65fd53fe57c517ea
msuudoct2@gmail.com <msuudoct2@gmail.com>
2024-05-17 14:41:29 +0530

    loginpage

--- src/components/Searchbar/SearchBar.jsx
+++ src/components/Searchbar/SearchBar.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
-import './SearchBar.css';
+import React from "react";
+import "./SearchBar.css";
 
 const SearchBar = () => {
   return (
-    <div className='container p-5'>
-      <div className='d-flex justify-content-center flex-column text-center'>
-       <div className='display-5 dark-text m-5'>Search for AI here</div>
+    <div className="container ">
+      <div className="d-flex justify-content-center flex-column text-center">
+        <div className="display-5 text-white m-5">Search for AI here</div>
         <div className="d-flex justify-content-center h-100">
           <div className="search">
-            <input className="search_input" type="text" placeholder="Search here..." />
-            <a href="#" className="search_icon">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+            <input
+              class="search_input"
+              type="text"
+              name=""
+              placeholder="Search here..."
+            />
+            <a href="#" class="search_icon">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-search"
+                viewBox="0 0 16 16"
+              >
                 <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
               </svg>
             </a>
@@ -19,6 +31,6 @@ const SearchBar = () => {
       </div>
     </div>
   );
-}   
+};
 
 export default SearchBar;
