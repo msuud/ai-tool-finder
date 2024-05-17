9a7d8e895f1fd73fc5c1664335f1795bdbf676ed
22IT090-Ansh <22it090@charusat.edu.in>
2024-05-17 13:36:06 +0530

    Searchbar

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./HomePage.css";
+import SearchBar from "../Searchbar/SearchBar";
 
 const HomePage = () => {
   return (
     <div className="container-fluid bg-image">
       <div className="container text-black ml-0"></div>
+      <SearchBar></SearchBar>
     </div>
   );
 };
